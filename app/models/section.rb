a23f599a5d7d1a22af71bbf72fe8128f3bbe4da4
Amana Cotcamp <amanacodercamp@gmail.com>
2017-08-30 17:53:54 -0300

    fixed admin section new

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,7 +2,7 @@ class Section < ApplicationRecord
   belongs_to :contentable, polymorphic: true, inverse_of: :sections
   belongs_to :lesson
 
-  delegate :title, to: :contentable, prefix: true
+  delegate :title, to: :contentable, prefix: true, allow_nil: true
 
   def title
     "#{contentable_type} - #{contentable_id} - #{contentable_title}"
