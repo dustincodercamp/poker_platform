eef81a92332cea388d3477ba0613981d7ab66f69
Amana Cotcamp <amanacodercamp@gmail.com>
2017-08-31 15:14:17 -0300

    added name to logo and linked exercises at the end of lessons

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,6 +1,7 @@
 class ExercisesController < ApplicationController
   def index
     @lesson = Lesson.find(params[:lesson_id])
+    @next_lesson = Lesson.find_by(order: @lesson.order + 1)
     @exercises = @lesson.exercises
   end
 end
